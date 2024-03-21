e56c16453f6befe437d4daaa35ea16a24ae8ad84
mayav02 <96153903+mayav02@users.noreply.github.com>
2024-03-20 17:17:35 -0700

    Update myhangman.py

--- myhangman.py
+++ myhangman.py
@@ -3,51 +3,25 @@ from words import words
 from hangman_visual import lives_visual_dict
 import string
 
-##Comp chooses word from word lis
-def choose_word(words):
-    for words in words:
-        chosen_word = random.choice(words)
-        while '-' or ' ' in words:
-            chosen_word = random.choice(words)
-    return chosen_word.upper()
-
-print("Let's play Hangman!")
-def play():
-    chosen_word = choose_word(words)
-    chosen_word_letters = set(chosen_word)
-    current_guesses = set()
-    uppercase_alphabet = string.ascii_uppercase
-    uppercase_alphabet = set()
-    user = input("Guess a letter: ")
-    lives = 7
-    print("Let's play Hangman!")
-          
-    while len(chosen_word) or lives >= 7:
-        print(f'You have {lives} left and you have used these letters:')
-        if ValueError:
-            print("That is not a valid letter")
-        if user not in chosen_word:
-            print("Your letter", user, "is not in the word")
-        else:
-            user.append(current_guesses)
-
-##chosen_word
-if __name__ == '__myhangman__':
-    play()
-
-
 ##Takes a list of words as a parameter
     ##Selects a random word from the list
     #Continue choosing words until
     ## a valid word without a dash or space is obtained
     ##Returns the word in all uppercase
+def choose_word(words):
+    chosen_word = random.choice(words)
+    print(chosen_word)
+    while '-' in chosen_word or ' ' in chosen_word:
+        print("while loop called, new word coming")
+        chosen_word = random.choice(words)
+        print(chosen_word)
+    return chosen_word.upper()
 
-
-    ##Calls the above function to obtain a valid word
-    ##Initializes sets for the letters in the word, the alphabet and used letters.
-    ##Sets the initial number of lives to 7.
-    ##While loop that continues as long as there are letters to guess.
-    ##And the lives remaining.
+##Calls the above function to obtain a valid word. YES
+    ##Initializes sets for the letters in the word, the alphabet and used letters. Y
+    ##Sets the initial number of lives to 7. Y
+    ##While loop that continues as long as there are letters to guess. Y
+    ##And the lives remaining. Y
         ##Words list where the guessed letters are displayed
         ##and unguessed letters are represented as '-'.
         ##Prints the hangman visual graphic and the current state of the word
@@ -61,3 +35,30 @@ if __name__ == '__myhangman__':
     ##Gets here when len(word_letters) == 0 OR when lives == 0
         ##Prints the hangman graphic and the word if lives run out
         ##Otherwise, congratulates the user for guessing the word.
+
+def play():
+    print("Let's Play Hangman!")
+    chosen_word = choose_word(words)
+    chosen_word_letters = set()
+    string.ascii_uppercase
+    uppercase_alphabet = list(string.ascii_uppercase)
+    dashes = len(chosen_word) * '-'
+    lives = 7
+    while len(chosen_word) or lives >= 7:
+        print("Current word:", dashes)
+        new_used_list = [user if user in chosen_word_letters else '-' for user in chosen_word]
+        print(f'You have {lives} lives left and you have used these letters:', new_used_list)
+        player = input("Guess a letter: ")
+        uppercase_player = player.upper()
+        print(uppercase_player)
+        if uppercase_player not in uppercase_alphabet:
+            print("That is not a valid letter.")
+        elif uppercase_player not in chosen_word:
+            print("Your letter", uppercase_player, "is not in the word")
+        else:
+            chosen_word_letters.add(uppercase_player)
+        print(chosen_word_letters)
+        lives -= 1
+
+
+play()
