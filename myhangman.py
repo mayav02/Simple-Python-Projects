2bcce6e0cd785fdabd5258bc661abeb3ba988e63
mayav02 <96153903+mayav02@users.noreply.github.com>
2024-03-22 15:52:26 -0700

    Update myhangman.py

--- myhangman.py
+++ myhangman.py
@@ -54,3 +54,68 @@ def play():
 
 
 play()
+
+
+
+
+
+"""
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
+
+def choose_word(words):
+    chosen_word = random.choice(words)
+    print(chosen_word, '\n')
+    while '-' in chosen_word or ' ' in chosen_word:
+        print("while loop called, new word coming")
+        chosen_word = random.choice(words)
+        ##print(chosen_word)
+    return chosen_word.upper()
+
+def play():
+    print('Hey Bestie!')
+    print('Lets Play Hangman! \n')
+    chosen_word = choose_word(words)
+    ##turn into a list
+    chosen_word_letters = list[chosen_word]
+    string.ascii_uppercase
+    uppercase_alphabet = list(string.ascii_uppercase)
+    dashes = len(chosen_word) * ' - '
+    split_list = list[chosen_word.sort()]
+    ##print('this is the split set:', split_set)
+
+    for i in range(1, 27) or (chosen_word_letters == split_list): 
+        ##^which equals 26, length of alphabet
+        ##print('chosen word letters: ',chosen_word_letters)
+        ##print('this is the split set:',split_set)
+        new_used_list = [user if user in chosen_word_letters else '-' for user in chosen_word]
+        print('You have used these letters:', (' '.join(chosen_word_letters)), '\n')
+        print("Current word:", (' '.join(new_used_list)))
+        player = input('Guess a letter: ')
+        uppercase_player = player.upper()
+        if uppercase_player not in uppercase_alphabet:
+            print('That is not a valid letter.' , '\n')
+        elif uppercase_player not in chosen_word:
+            print(f'Your letter, {uppercase_player}, is not in the word.','\n' )
+            chosen_word_letters.append(uppercase_player)
+        else:
+            print('Yes! That is right!', '\n')
+            chosen_word_letters.append(uppercase_player)
+  
+        if len(chosen_word_letters) == 26:
+            print("Sorry, you have no more lives left.")
+            print(f"The word was {chosen_word}")
+            print("Play Again!")
+
+        if new_used_list == split_list:
+            print("Chosen word letters: ", chosen_word_letters)
+            print(f"Congratulations! You guessed {chosen_word} correctly!")
+            print("Play Again!")
+            break
+
+
+play()
+
+"""
