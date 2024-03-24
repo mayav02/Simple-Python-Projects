59cb0eeb1138deb1bc7f664965318618eb83332f
mayav02 <96153903+mayav02@users.noreply.github.com>
2024-03-24 15:29:07 -0700

    Update myhangman.py

--- myhangman.py
+++ myhangman.py
@@ -1,11 +1,10 @@
 import random
 from words import words
-from hangman_visual import lives_visual_dict
 import string
 
 def choose_word(words):
     chosen_word = random.choice(words)
-    print(chosen_word, '\n')
+    ##print(chosen_word, '\n')
     while '-' in chosen_word or ' ' in chosen_word:
         print("while loop called, new word coming")
         chosen_word = random.choice(words)
@@ -16,104 +15,58 @@ def play():
     print('Hey Bestie!')
     print('Lets Play Hangman! \n')
     chosen_word = choose_word(words)
-    chosen_word_letters = set()
+    final_chosen_word = sorted(chosen_word)
+    chosen_word_letters = []
     string.ascii_uppercase
-    uppercase_alphabet = list(string.ascii_uppercase)
-    dashes = len(chosen_word) * ' - '
-    split_set = set(chosen_word)
-    ##print('this is the split set:', split_set)
+    uppercase_alphabet = set(string.ascii_uppercase)
 
-    for i in range(1, 27) or (chosen_word_letters == split_set): 
-        ##^which equals 26, length of alphabet
-        ##print('chosen word letters: ',chosen_word_letters)
-        ##print('this is the split set:',split_set)
+    attempts = 0
+    max_attempts = 26
+
+    while True:
+        attempts += 1
+        
         new_used_list = [user if user in chosen_word_letters else '-' for user in chosen_word]
+        final_new_used = sorted(new_used_list)
+        final_chosen_word_letters = sorted(chosen_word_letters)
+
+        ##print("sorted final_new_used: " , final_new_used)
+        ##print("sorted final_chosen_word_letters: ", final_chosen_word_letters)
+
         print(f'You have used these letters:', (' '.join(chosen_word_letters)), '\n')
         print("Current word:", (' '.join(new_used_list)))
+
         player = input('Guess a letter: ')
+        print('\n')
         uppercase_player = player.upper()
+
         if uppercase_player not in uppercase_alphabet:
             print('That is not a valid letter.' , '\n')
         elif uppercase_player not in chosen_word:
             print(f'Your letter, {uppercase_player}, is not in the word.','\n' )
-            chosen_word_letters.add(uppercase_player)
+            chosen_word_letters.append(uppercase_player)
+        elif uppercase_player in chosen_word_letters:
+            print("You have already guessed that letter")
+            print("Try again.", '\n')
+            chosen_word_letters.append(uppercase_player)
         else:
             print('Yes! That is right!', '\n')
-            chosen_word_letters.add(uppercase_player)
+            chosen_word_letters.append(uppercase_player)
+
+        if final_new_used == final_chosen_word:
+            print(f"Congratulations! You guessed {chosen_word} correctly!")
+            print("Play Again!")
+            break
   
-        if len(chosen_word_letters) == 26:
+        if attempts == max_attempts:
             print("Sorry, you have no more lives left.")
             print(f"The word was {chosen_word}")
-            print("Play Again!")
-
-        if new_used_list == split_set:
-            print("Chosen word letters: ", chosen_word_letters)
-            print(f"Congratulations! You guessed {chosen_word} correctly!")
             print("Play Again!")
             break
 
 
 play()
 
-
-
-
-
-"""import random
-from words import words
-from hangman_visual import lives_visual_dict
-import string
-
-def choose_word(words):
-    chosen_word = random.choice(words)
-    print(chosen_word, '\n')
-    while '-' in chosen_word or ' ' in chosen_word:
-        print("while loop called, new word coming")
-        chosen_word = random.choice(words)
-        ##print(chosen_word)
-    return chosen_word.upper()
-
-def play():
-    print('Hey Bestie!')
-    print('Lets Play Hangman! \n')
-    chosen_word = choose_word(words)
-    chosen_word_letters = []
-    ##chosen_word_letters.sort(chosen_word)
-    string.ascii_uppercase
-    uppercase_alphabet = set(string.ascii_uppercase)
-    dashes = len(chosen_word) * ' - '
-    new_used_list = []
-    ##split_set = set(chosen_word)
-    ##print('this is the split set:', split_set)
-
-    for i in range(1, 27) or (chosen_word_letters == new_used_list): 
-        ##^which equals 26, length of alphabet
-        new_used_list = [user if user in chosen_word_letters else '-' for user in chosen_word]
-        print('chosen word letters: ',chosen_word_letters)
-        print('this is new used list: ', new_used_list)
-        print(f'You have used these letters:', (' '.join(chosen_word_letters)), '\n')
-        print("Current word:", (' '.join(new_used_list)))
-        player = input('Guess a letter: ')
-        uppercase_player = player.upper()
-        if uppercase_player not in uppercase_alphabet:
-            print('That is not a valid letter.' , '\n')
-        elif uppercase_player not in chosen_word:
-            print(f'Your letter, {uppercase_player}, is not in the word.','\n' )
-            chosen_word_letters.append(uppercase_player)
-        else:
-            print('Yes! That is right!', '\n')
-            chosen_word_letters.append(uppercase_player)
-  
-        if len(chosen_word_letters) == 26:
-            print("Sorry, you have no more lives left.")
-            print(f"The word was {chosen_word}")
-            print("Play Again!")
-
-        if new_used_list == chosen_word_letters:
-            print("Chosen word letters: ", chosen_word_letters)
-            print(f"Congratulations! You guessed {chosen_word} correctly!")
-            print("Play Again!")
-            break
 
 
 play()
